beebec2bb5d5aec805b4ba4d5070d3d60096e9aa
babaAH <shama_chich@icloud.com>
2021-01-24 12:55:04 +0300

    first commit

---
+++ database/migrations/2021_01_24_095003_create_departments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger("director_id");
+            $table->timestamps();
+
+            $table->foreign('director_id')->references('id')->on('users')->onDelete("cascade");
+            // $table->foreign('deputy_chef_id')->references('id')->on('users')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
